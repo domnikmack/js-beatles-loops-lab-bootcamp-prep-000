f2d5275937cd0b6eea279e7112dbc31e48124026
Domnik Mack <domnikmack@gmail.com>
2017-09-25 00:53:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay (musicians, instruments) {
   var whoPlaysWhat = [];
   for ( var i = 0; i <4; i++) {
-    theBeatlesPlay.push(musicians[i] + ' plays ' + instruments[i]);
+    whoPlaysWhat.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return theBeatlesPlay;
 }
